b50c947cf70f6e47444a0577f7f2f83a58063af2
= <=>
2024-03-12 11:12:39 +0600

    Blog, Contact, Features content page

--- resources/js/Pages/Blog.vue
+++ resources/js/Pages/Blog.vue
@@ -1,11 +1,156 @@
 <script setup>
 import Breadcrumb from "@components/Breadcrumb.vue";
-
-defineProps({ list: Array });
 </script>
 
 <template>
   <Breadcrumb title="BLOGS" />
 
-  <div>BLOGS</div>
+  <section class="section">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-4">
+          <div class="post-entry">
+            <a href="blog-single.html" class="d-block mb-4">
+              <img src="@asset/img/img_1.jpg" alt="Image" class="img-fluid" />
+            </a>
+            <div class="post-text">
+              <span class="post-meta"
+                >December 13, 2019 &bullet; By <a href="#">Admin</a></span
+              >
+              <h3>
+                <a href="#"
+                  >Chrome now alerts you when someone steals your password</a
+                >
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
+                optio.
+              </p>
+              <p><a href="#" class="readmore">Read more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="post-entry">
+            <a href="blog-single.html" class="d-block mb-4">
+              <img src="@asset/img/img_2.jpg" alt="Image" class="img-fluid" />
+            </a>
+            <div class="post-text">
+              <span class="post-meta"
+                >December 13, 2019 &bullet; By <a href="#">Admin</a></span
+              >
+              <h3>
+                <a href="#"
+                  >Chrome now alerts you when someone steals your password</a
+                >
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
+                optio.
+              </p>
+              <p><a href="#" class="readmore">Read more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="post-entry">
+            <a href="blog-single.html" class="d-block mb-4">
+              <img src="@asset/img/img_3.jpg" alt="Image" class="img-fluid" />
+            </a>
+            <div class="post-text">
+              <span class="post-meta"
+                >December 13, 2019 &bullet; By <a href="#">Admin</a></span
+              >
+              <h3>
+                <a href="#"
+                  >Chrome now alerts you when someone steals your password</a
+                >
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
+                optio.
+              </p>
+              <p><a href="#" class="readmore">Read more</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="post-entry">
+            <a href="blog-single.html" class="d-block mb-4">
+              <img src="@asset/img/img_4.jpg" alt="Image" class="img-fluid" />
+            </a>
+            <div class="post-text">
+              <span class="post-meta"
+                >December 13, 2019 &bullet; By <a href="#">Admin</a></span
+              >
+              <h3>
+                <a href="#"
+                  >Chrome now alerts you when someone steals your password</a
+                >
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
+                optio.
+              </p>
+              <p><a href="#" class="readmore">Read more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="post-entry">
+            <a href="blog-single.html" class="d-block mb-4">
+              <img src="@asset/img/img_3.jpg" alt="Image" class="img-fluid" />
+            </a>
+            <div class="post-text">
+              <span class="post-meta"
+                >December 13, 2019 &bullet; By <a href="#">Admin</a></span
+              >
+              <h3>
+                <a href="#"
+                  >Chrome now alerts you when someone steals your password</a
+                >
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
+                optio.
+              </p>
+              <p><a href="#" class="readmore">Read more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="post-entry">
+            <a href="blog-single.html" class="d-block mb-4">
+              <img src="@asset/img/img_2.jpg" alt="Image" class="img-fluid" />
+            </a>
+            <div class="post-text">
+              <span class="post-meta"
+                >December 13, 2019 &bullet; By <a href="#">Admin</a></span
+              >
+              <h3>
+                <a href="#"
+                  >Chrome now alerts you when someone steals your password</a
+                >
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
+                optio.
+              </p>
+              <p><a href="#" class="readmore">Read more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 text-center">
+          <span class="p-3 active text-primary">1</span>
+          <a href="#" class="p-3">2</a>
+          <a href="#" class="p-3">3</a>
+          <a href="#" class="p-3">4</a>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
--- resources/js/Pages/Features.vue
+++ resources/js/Pages/Features.vue
@@ -1,11 +1,103 @@
 <script setup>
 import Breadcrumb from "@components/Breadcrumb.vue";
-
-defineProps({ list: Array });
 </script>
 
 <template>
   <Breadcrumb title="FEATURES" />
 
-  <div>FEATURES</div>
+  <section class="section pb-0">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4 me-auto">
+          <h2 class="mb-4">Seamlessly Communicate</h2>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at
+            reprehenderit optio, laudantium eius quod, eum maxime molestiae
+            porro omnis. Dolores aspernatur delectus impedit incidunt dolore
+            mollitia esse natus beatae.
+          </p>
+          <p><a href="#">Read More</a></p>
+        </div>
+        <div class="col-md-6" data-aos="fade-left">
+          <img
+            src="@asset/img/undraw_svg_2.svg"
+            alt="Image"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4 ms-auto order-2">
+          <h2 class="mb-4">Gather Feedback</h2>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at
+            reprehenderit optio, laudantium eius quod, eum maxime molestiae
+            porro omnis. Dolores aspernatur delectus impedit incidunt dolore
+            mollitia esse natus beatae.
+          </p>
+          <p><a href="#">Read More</a></p>
+        </div>
+        <div class="col-md-6" data-aos="fade-right">
+          <img
+            src="@asset/img/undraw_svg_3.svg"
+            alt="Image"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section pb-0">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4 me-auto">
+          <h2 class="mb-4">Present Designs Inspiration</h2>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at
+            reprehenderit optio, laudantium eius quod, eum maxime molestiae
+            porro omnis. Dolores aspernatur delectus impedit incidunt dolore
+            mollitia esse natus beatae.
+          </p>
+          <p><a href="#">Read More</a></p>
+        </div>
+        <div class="col-md-6" data-aos="fade-left">
+          <img
+            src="@asset/img/undraw_svg_3.svg"
+            alt="Image"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4 ms-auto order-2">
+          <h2 class="mb-4">Powerful App Design</h2>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at
+            reprehenderit optio, laudantium eius quod, eum maxime molestiae
+            porro omnis. Dolores aspernatur delectus impedit incidunt dolore
+            mollitia esse natus beatae.
+          </p>
+          <p><a href="#">Read More</a></p>
+        </div>
+        <div class="col-md-6" data-aos="fade-right">
+          <img
+            src="@asset/img/undraw_svg_4.svg"
+            alt="Image"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
